3cd453e72b8f0100507e0006c80e35963608f8dd
betatampw <betatampw@gmail.com>
2014-01-13 20:43:06 +0400

    marker

--- files/js/script.js
+++ files/js/script.js
@@ -1,4 +1,4 @@
-function showHideBtnScroll() {
+/*function showHideBtnScroll() {
 	if (parseInt($(window).scrollTop()) > 100) {
 		$('#btnScrollTop').fadeIn(400);
 	}
@@ -8,7 +8,7 @@ function showHideBtnScroll() {
 }
 window.onscroll = function() {
 	showHideBtnScroll();
-}
+}*/
 /*
 $("a[href^=#]").click(function(){
 	event.preventDefault();
@@ -19,5 +19,27 @@ $("a[href^=#]").click(function(){
 });	
 */
 
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('YMapsID', {
+            center: [55.751574, 37.573856],
+            zoom: 9
+        }),
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: '../images/marker.png',
+            // Размеры метки.
+            iconImageSize: [30, 42],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-3, -42]
+        });
+
+    myMap.geoObjects.add(myPlacemark);
+});
 
 	
